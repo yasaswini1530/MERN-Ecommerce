95fa68ff511ef03d9822eb14a974c1dce0795b54
yasaswini1530 <238w1a12b1@vrsec.ac.in>
2026-04-17 16:26:32 +0530

    complete

--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -14,7 +14,7 @@ router.post("/add",protect,async (req,res)=>{
             console.log("if block",cart)
         }else{
             console.log("from else",cart)
-            const itemIndex=cart.item.find(item=>item.productId.toString()==productId)
+            const itemIndex=cart.item.findIndex(item=>item.productId.toString()==productId)
             if(itemIndex>-1){
                 cart.item[itemIndex].quantity+=1
             }
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,84 +1,34 @@
-import { useEffect, useState } from "react";
-import API from "../api/axios";
-import {useNavigate} from 'react-router-dom'
-function Home() {
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const navigate=useNavigate()
-  const role=localStorage.getItem("role")
-  useEffect(() => {
-    fetchProducts();
-  }, []);
+import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Register from './components/Register'
+import Login from './components/Login'
+import Addproduct from './components/Addproduct'
+import Home from './components/Home'
+import ProtectedRoute from './components/ProtectedRoute'
+import Cart from './components/Cart'
 
-  const fetchProducts = async () => {
-    try {
-      const res = await API.get("/product");
-      setProducts(res.data);
-      console.log(res.data)
-      navigate("/cart")
-    } catch (err) {
-      console.log(err);
-    } finally {
-      setLoading(false);
-    }
-  };
 
-  const addToCart = async (id) => {
-    API.post("/cart/add",{productId:id})
-      .then((res)=>{
-        if(res.status==201){
-          alert("Added to cart")
-          navigate("/cart")
-        }
-      })
-      .catch((err)=>{
-        console.log(err)
-      })
-  };
-
+export default function App() {
   return (
-    <div className="container mt-5">
-      <h2>Products</h2>
+    <BrowserRouter>
+    <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+        <Route path="/add-product" element={
+          <ProtectedRoute>
+            <Addproduct/>
+          </ProtectedRoute>
+        }/>
+        <Route path="/cart" element={
+          <ProtectedRoute>
+            <Cart/>
+          </ProtectedRoute>
+        }/>
 
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        <div className="row">
-          {products.length === 0 ? (
-            <p>No products available</p>
-          ) : (
-            products.map((p) => (
-              <div className="col-md-3" key={p._id}>
-                <div className="card mb-3 px-3">
-                  <img
-                    src={p.image}
-                    className="card-img-top"
-                    alt={p.name}
-                    style={{ height: "200px", objectFit: "cover" }}
-                  />
-
-                  <div className="card-body">
-                    <h5>{p.name}</h5>
-                    <p>₹{p.price}</p>
-                    <p>{p.description}</p>
-                    {
-                      role=="user"&&<button
-                                          className="btn btn-success w-100"
-                                          onClick={() => addToCart(p._id)}
-                                        >
-                                          Add to Cart
-                                        </button>
-                    }
-                    
-                  </div>
-                </div>
-              </div>
-            ))
-          )}
-        </div>
-      )}
-    </div>
-  );
+      </Routes>
+    </BrowserRouter>
+  )
 }
-
-export default Home;
